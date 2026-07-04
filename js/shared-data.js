5c20dd0173caed70f2cf3f0028cdc3682e57e666
SamoVR <samuelbrnak@gmail.com>
2026-07-04 19:36:48 +0200

    lockdown on

--- js/shared-data.js
+++ js/shared-data.js
@@ -365,7 +365,7 @@ const DEFAULT = {
       }
     ],
     "cursorEffect": true,
-    "lockdown": false,
+    "lockdown": true,
     "lockdownMsg": "This portfolio is currently private. Check back soon.",
     "scrollReveal": true,
     "heroReveal": true,
